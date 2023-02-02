e23417a1ffa149005c2d60f96cfd3e46d242b81c
om <omjio3823@gmail.com>
2023-02-02 22:49:44 +0530

    update

--- src/pattern_Q4_TT.java
+++ src/pattern_Q4_TT.java
@@ -8,25 +8,21 @@
 
 public class pattern_Q4_TT {
     public static void main(String[] args) {
-        int a=5;
+        int a = 5;
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
-                if(i%2==0) {
-                    if(j%2==0) {
+                if (i % 2 == 0) {
+                    if (j % 2 == 0) {
                         System.out.print("1");
-                    }
-                    else{
+                    } else {
                         System.out.print("0");
                     }
-                }
-                else{
-                    if(j%2==0) {
+                } else {
+                    if (j % 2 == 0) {
                         System.out.print("0");
-                    }
-                    else{
+                    } else {
                         System.out.print("1");
                     }
-
                 }
             }
             System.out.println();
