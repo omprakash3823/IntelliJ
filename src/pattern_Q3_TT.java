ae96dbb1abdb5a26642532bac76de1807911e636
om <omjio3823@gmail.com>
2023-02-04 02:04:56 +0530

    update

--- src/pattern_Q3_TT.java
+++ src/pattern_Q3_TT.java
@@ -8,33 +8,16 @@
 
 public class pattern_Q3_TT {
     public static void main(String[] args) {
-        int a = 5;
-        for (int i = 0; i < a; i++) {
-            if (i == 0 || i == (a - 1)) {
-                for (int j = 0; j < 7; j++) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++)
+                if (i == 0 || i == 5 - 1 || j == 0 || j == (5 - 1)) {
                     System.out.print("*");
-
-                }
-                System.out.println();
-            } else if (i == 2) {
-                for (int j = 0; j < 7; j++) {
-                    if (j == 0 || j == 3 || j == 6) {
-                        System.out.print("*");
-                    } else {
-                        System.out.print(" ");
-                    }
-                }
-                System.out.println();
-            } else {
-                for (int j = 0; j < 7; j++) {
-                    if (j == 0 || j == (7 - 1)) {
-                        System.out.print("*");
-                    } else {
-                        System.out.print(" ");
-                    }
-                }
-                System.out.println();
-            }
+                } else if (i == 5 / 2 && j == 5 / 2) {
+                    System.out.print("*");
+                } else System.out.print(" ");
+            System.out.println();
         }
+
     }
 }
+
