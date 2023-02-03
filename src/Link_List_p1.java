75a007113d53a2406a3b78db26c57c096ae5c5f8
om <omjio3823@gmail.com>
2023-02-03 23:33:58 +0530

    update

---
+++ src/Link_List_p1.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+class node{
+    int value;
+    node next;
+    node(){
+         value=0;
+         next=null;
+
+    }
+}
+public class Link_List_p1 {
+    public static void main(String[] args) {
+
+    }
+
+}
